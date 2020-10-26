cf438dc6b4b1e17ac454eae9ded86e5876c279f9
Dzianis Babuskin <dzianis_babuskin@epam.com>
2020-10-26 03:08:14 -0700

    private key fix

--- src/vis_client_example.py
+++ src/vis_client_example.py
@@ -16,9 +16,9 @@
 import time
 import logging
 
-import requests
-
-from aos_vis_client import VISClient, VISDataSubscription, VISDataAccessor
+# import requests
+#
+# from aos_vis_client import VISClient, VISDataSubscription, VISDataAccessor
 
 logger = logging.getLogger(__name__)
 
@@ -34,38 +34,9 @@ DELAY_AFTER_ERROR = 2
 
 
 def main():
-    with VISClient(VIS_URL) as client:
-        # Initialize data accessor to "VIN" attribute and get this attribute.
-        vin_accessor = VISDataAccessor(path="Attribute.Vehicle.VehicleIdentification.VIN")
-        client.register_vis_data(vin_accessor)
-        vin_accessor.send_get_action()
-        vin = vin_accessor.get_value(wait_timeout=WAIT_TIMEOUT)
-
-        # Initialize telemetry info subscription.
-        telemetry_sub = VISDataSubscription("Signal.Emulator.telemetry.*")
-        client.register_vis_data(telemetry_sub)
-        telemetry_sub.send_subscribe_action()
-
-        # Send information received from VIS to HTTP server.
-        while True:
-            try:
-                logger.info("Sending telemetry to '{url}'".format(url=HTTP_REQUEST_RECEIVER_URL))
-                requests.post(
-                    url=HTTP_REQUEST_RECEIVER_URL,
-                    json={"vin": vin, "telemetry": telemetry_sub.get_value(wait_timeout=WAIT_TIMEOUT)}
-                )
-
-                time.sleep(DATA_SENDING_DELAY)
-            except KeyboardInterrupt:
-                logger.info("Received Keyboard interrupt. shutting down")
-                break
-            except Exception as exc:
-                logger.error(
-                    "Unhandled exception: {exc_name}".format(exc_name=exc.__class__.__name__),
-                    exc_info=True
-                )
-                time.sleep(DELAY_AFTER_ERROR)
-                continue
+    while True:
+        print('Hello world!')
+        time.sleep(3)
 
 
 if __name__ == '__main__':
